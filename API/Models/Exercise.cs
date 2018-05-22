d22d0fd0df9552dfdc93b897738615db8f3d723d
Chung2 <chungkeuwong@gmail.com>
2018-05-22 20:48:10 +0200

    added models
    exercise.cs, exerciseresult.cs and exerciserecording.cs

---
+++ API/Models/Exercise.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class Exercise
+    {
+
+        public long ID { get; set; }
+
+        [Required]
+        public DateTime Startdate { get; set; }
+        [Required]
+        private DateTime Enddate { get; set; }
+
+        [Required]
+        public string User_id { get; set; }
+        [ForeignKey("User_ID")]
+
+        public User User { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+        [Required]
+        public int Amount { get; set; }
+        [Required]
+        public int ExerciseRecording_ID { get; set; }
+        [ForeignKey("ExerciseRecording_ID")]
+
+        public ExerciseRecording ExerciseRecording { get; set; }
+
+    }
+}
---
+++ API/Models/ExerciseRecording.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+
+    public class ExerciseRecording
+    {
+
+        public int ID { get; set; }
+
+        [Required]
+        // moet blob zijn
+        public string Recording { get; set; }
+
+    }
+}
