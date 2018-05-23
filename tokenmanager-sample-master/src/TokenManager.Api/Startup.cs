4d2870d7e43512ff9b6deb77e8eb46d94eafae58
Nick W <nickwindt@hotmail.nl>
2018-05-23 16:42:03 +0200

    Actually added the example projects

---
+++ tokenmanager-sample-master/src/TokenManager.Api/Startup.cs
@@ -0,0 +1,66 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using TokenManager.Api.Models;
+using TokenManager.Api.Services;
+
+namespace TokenManager.Api
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddLogging();
+            services.AddSingleton<IAccountService,AccountService>();
+            services.AddSingleton<IJwtHandler,JwtHandler>();
+            services.AddSingleton<IPasswordHasher<User>,PasswordHasher<User>>();
+            services.AddTransient<TokenManagerMiddleware>();
+            services.AddTransient<ITokenManager, Services.TokenManager>();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddDistributedRedisCache(r => { r.Configuration = Configuration["redis:connectionString"]; });
+            var jwtSection = Configuration.GetSection("jwt");
+            var jwtOptions = new JwtOptions();
+            jwtSection.Bind(jwtOptions);
+            services.AddAuthentication()
+                .AddJwtBearer(cfg => 
+                {
+                    cfg.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.SecretKey)),
+                        ValidIssuer = jwtOptions.Issuer,
+                        ValidateAudience = false,
+                        ValidateLifetime = true
+                    };
+                });
+            services.Configure<JwtOptions>(jwtSection);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,
+            ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddDebug().AddConsole();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseMiddleware<ErrorHandlerMiddleware>();
+            app.UseAuthentication();
+            app.UseMiddleware<TokenManagerMiddleware>();
+            app.UseMvc();
+        }
+    }
+}
